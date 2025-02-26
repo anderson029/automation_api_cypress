49c156a931de58543b93e0d5f83a0036c5b80730
anderson029 <andersonfoliveira@yahoo.com>
2025-02-26 10:19:17 -0300

    remove comments

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,12 +5,10 @@ import { buildLoginPayload } from '../support/payloadBuilder';
 Cypress.Commands.add('login', () => {
   const LOGIN_ENDPOINT = `${Cypress.env('BASE_URL')}/accountservice/accountrest/api/v1/login`;
 
-  // Obtém as credenciais do usuário
   return cy.task('getUserData').then((userData) => {
     cy.log('Dados do usuário:', JSON.stringify(userData));
     const credentials = buildLoginPayload(userData);
 
-    // Faz a requisição de login
     return cy.api({
       url: LOGIN_ENDPOINT,
       method: 'POST',
@@ -19,7 +17,6 @@ Cypress.Commands.add('login', () => {
       expect(response.status).to.eql(200);
       expect(response.body.statusMessage.reason).to.eql('Login Successful');
 
-      // Armazena o token e userId no Cypress.env()
       Cypress.env('authToken', response.body.statusMessage.token);
       Cypress.env('userId', response.body.statusMessage.userId);
     });
